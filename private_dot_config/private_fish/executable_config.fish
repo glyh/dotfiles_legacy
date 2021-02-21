359eb22ff64754fbb5c226c085fb0a4c5ccc421d
lyhokia <lyhokia@gmail.com>
2021-02-21 10:07:18 +0800

    update dotfiles

--- private_dot_config/private_fish/executable_config.fish
+++ private_dot_config/private_fish/executable_config.fish
@@ -1,3 +1,23 @@
 set -a PATH ~/.local/bin
 set -a PATH ~/.nimble/bin
 set XDG_USER_CONFIG_DIR ~/.config
+
+alias ed="nvim"
+alias pa="paru"
+alias ch="chezmoi"
+alias lr="lein repl"
+
+function expand-dot-to-parent-directory-path -d 'expand ... to ../.. etc'
+    # Get commandline up to cursor
+    set -l cmd (commandline --cut-at-cursor)
+
+    # Match last line
+    switch $cmd[-1]
+        case '*..'
+            commandline --insert '/..'
+        case '*'
+            commandline --insert '.'
+    end
+end
+
+bind . 'expand-dot-to-parent-directory-path'
